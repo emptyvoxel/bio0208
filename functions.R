0d22a5bd56fd683a1f4b3e7bf6c408dfc6b66f8f
Gabriel <89980949+emptyvoxel@users.noreply.github.com>
2025-12-08 21:26:20 -0300

    Adiciona comparação geracional e sexual

--- functions.R
+++ functions.R
@@ -1,12 +1,64 @@
 # Helper functions para a P2 de Processos
 library("ggplot2")
+library("tidyr")
 library("dplyr")
 library("gridExtra")
 
 dataset <- read.csv("dataset.csv")
 
-# Questão 1 (orquestra porque são vários violinos)
-orchestra <- function(df, caractere, ylab) {
+# ======================================================================
+# Questão 1 - Caracteres, Sexo e Gerações
+# ======================================================================
+# Compara o peso entre sexos e gerações
+peso_sexo_geracao <- function(df, ylab) {
+  df_long <- df %>%
+    mutate(familia = row_number()) %>%
+    pivot_longer(
+      cols = c(pesoFilial, pesoMae, pesoPai, pesoMaeJovem, pesoPaiJovem),
+      names_to = "variavel",
+      values_to = "valor"
+    ) %>%
+    mutate(
+      group = case_when(
+        variavel == "pesoFilial" & sexo == "F" ~ "F1 F",
+        variavel == "pesoMaeJovem" ~ "F0 Jovem F",
+        variavel == "pesoMae" ~ "F0 F",
+        variavel == "pesoFilial" & sexo == "M" ~ "F1 M",
+        variavel == "pesoPaiJovem" ~ "F0 Jovem M",
+        variavel == "pesoPai" ~ "F0 M"
+      )
+    ) %>%
+    mutate(
+      sex_group = case_when(
+        group %in% c("F1 F", "F0 F", "F0 Jovem F") ~ "F",
+        group %in% c("F1 M", "F0 M", "F0 Jovem M") ~ "M"
+      )
+    )
+  
+  df_long$group <- factor(
+    df_long$group,
+    levels = c(
+      "F1 F", "F0 Jovem F", "F0 F",
+      "F1 M", "F0 Jovem M", "F0 M"
+    )
+  )
+  
+  plot <- ggplot(df_long, aes(x=group, y=valor, fill=sex_group)) +
+    geom_violin(trim=FALSE, alpha=.4) +
+    geom_boxplot(alpha=.8, outlier.alpha=.5) +
+    theme_minimal(base_size=14) +
+    coord_cartesian(ylim = c(20, 150)) + # Remove um outlier da *visualização* 
+    scale_fill_manual(
+      name = "Sexo",
+      values = c("F" = "tomato", "M" = "steelblue")
+    )  +
+    labs(x="Grupos", y="Peso")
+  
+  ggsave(ggsave("./images/peso entre sexos e gerações.png", plot, width=10, height=8, dpi=200))
+}
+
+# Compara o valor de um caractere entre os sexos
+caracteres_por_sexo <- function(df, caractere, ylab) {
   # Cria uma coluna temporária chamada "Total" agrupando os dois sexos
   temp <- bind_rows(df, df %>% mutate(sexo="Total"))
   
@@ -22,21 +74,22 @@ orchestra <- function(df, caractere, ylab) {
     )
 }
 
-q1_violins <- function() {
+# Plota a comparação de todos os caracteres entre os sexos
+sexo_f1_plots <- function(df) {
   # Gera o plot para cada caractere
-  altura <- orchestra(dataset, "alturaFilial", ylab="Altura")
-  peso <- orchestra(dataset, "pesoFilial", ylab="Peso")
-  pe <- orchestra(dataset, "peFilial", ylab="Comprimento do Pé")
-  pesoNasc <- orchestra(dataset, "pesoNascFilial", ylab="Peso ao Nascimento")
+  altura <- caracteres_por_sexo(df, "alturaFilial", ylab="Altura")
+  peso <- caracteres_por_sexo(df, "pesoFilial", ylab="Peso")
+  pe <- caracteres_por_sexo(df, "peFilial", ylab="Comprimento do Pé")
+  pesoNasc <- caracteres_por_sexo(df, "pesoNascFilial", ylab="Peso ao Nascimento")
 
   # Exibe todos os plots em um grid 2x2
   plot <- grid.arrange(altura, peso, pe, pesoNasc, ncol = 2)
   ggsave("./images/comparação-médias.png", plot, width=10, height=8, dpi=200)
 }
 
-q1_violins()
-
+# ======================================================================
 # Questão 3 - Herdabilidade
+# ======================================================================
 herdabilidade <- function(df, parental, filial, xlab, ylab) {
   f <- as.formula(paste(filial, "~", parental)) # filial ~ parental
   model <- lm(f, data=df)
